c2b2b1b2629212b63527bf8c050c57e6fab662b0
Laura J <43211954+matchacode@users.noreply.github.com>
2019-04-08 19:08:29 -0300

    Update users.js

--- src/model/users.js
+++ src/model/users.js
@@ -1,31 +1,26 @@
-const PostgresUtil = require('../utils/PostgresUtil')
-const bcrypt = require('bcrypt-nodejs')
+nst PostgresUtil = require('../utils/PostgresUtil')
 
-async function createUserTable() {
-  return await PostgresUtil.pool.query(`CREATE TABLE app_users (
-    handle         VARCHAR(100) PRIMARY KEY,
-    password_hash  VARCHAR(200)
+async function createLikesTable() {
+  return await PostgresUtil.pool.query(`CREATE TABLE likes (
+    message_id  INTEGER,
+    created_by  VARCHAR(200)
   )`)
 }
 
-async function createUser(handle, password) {
+async function createLikes(message_id, created_by) {
   try {
-    const passwordHash = bcrypt.hashSync(password)
-
     const result = await PostgresUtil.pool.query(
-      'INSERT INTO app_users VALUES ($1::text, $2::text);',
+      'INSERT INTO likes (message_id, created_by) VALUES ($1, $2);',
       [
-        handle, passwordHash
+        message_id, created_by
       ])
 
     return result
   } catch (exception) {
     if (exception.code === '42P01') {
       // 42P01 - table is missing - we'll create it and try again
-      await createUserTable()
-      return createUser(handle, password)
-    } else if (exception.code === '23505') {
-      throw new Error(`User ${handle} already exists`)
+      await createLikesTable()
+      return createLikesTable(handle, data)
     } else {
       // unrecognized, throw error to caller
       console.error(exception)
@@ -34,37 +29,17 @@ async function createUser(handle, password) {
   }
 }
 
-async function getUser(handle) {
+async function getLikes() {
   try {
     const result = await PostgresUtil.pool.query(
-      'SELECT * FROM app_users WHERE handle = $1::text',
-      [ handle ])
-
-    return result.rows[0]
-  } catch (exception) {
-    if (exception.code === '42P01') {
-      // 42P01 - table is missing - we'll create it and try again
-      await createUserTable()
-      return getUser(handle)
-    } else {
-      // unrecognized, throw error to caller
-      console.error(exception)
-      throw exception
-    }
-  }
-}
-
-async function getUsers() {
-  try {
-    const result = await PostgresUtil.pool.query(
-      'SELECT * FROM app_users')
+      'SELECT * FROM likes')
 
     return result.rows
   } catch (exception) {
     if (exception.code === '42P01') {
       // 42P01 - table is missing - we'll create it and try again
-      await createUserTable()
-      return getUsers()
+      await createMessageTable()
+      return getLikes()
     } else {
       // unrecognized, throw error to caller
       console.error(exception)
@@ -73,31 +48,8 @@ async function getUsers() {
   }
 }
 
-async function validateUser(handle, password) {
-  // look up user with the passed handle
-  const user = await getUser(handle)
-
-  // determine if we found a user with that handle
-  if (!user) {
-    throw new Error(`no user with handle ${handle}`)
-  }
-
-  // check that the password matches the one used to create the hash
-  const passwordHash = user.password_hash
-  if (!passwordHash) {
-    console.log('foundUser:', user)
-    throw new Error('password hash not found - time for a database refresh?')
-  }
-  if (!bcrypt.compareSync(password, passwordHash)) {
-    throw new Error('incorrect password')
-  }
-}
-
 module.exports = {
-  createUser: createUser,
-  getUser: getUser,
-  getUsers: getUsers,
-  validateUser: validateUser,
-  // TODO: read from environment variable
-  JWT_SECRET: 'There once was a person studying at the NSCC',
+  createLikes: createLikes,
+  createLikesTable: createLikesTable,
+  getLikes: getLikes,
 }
