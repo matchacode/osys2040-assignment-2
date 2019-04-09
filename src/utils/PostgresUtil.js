60e8181494342e0ffebf2ad3197610b3f9aaa5df
Laura J <43211954+matchacode@users.noreply.github.com>
2019-04-09 13:07:30 -0300

    Update PostgresUtil.js

--- src/utils/PostgresUtil.js
+++ src/utils/PostgresUtil.js
@@ -2,12 +2,12 @@ const { Pool } = require('pg')
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  // ssl: true,
-    user: "chatapp",
-   host: "localhost",
-   database: "postgres",
-   password: "student",
-   port: "5432"
+  ssl: true,
+    //user: "chatapp",
+  // host: "localhost",
+  // database: "postgres",
+  // password: "student",
+  // port: "5432"
 })
 
 module.exports = {
